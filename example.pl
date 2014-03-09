c64c484a2b67ecda6db5e06a70da089db195c1b2
Steve Kemp <steve@steve.org.uk>
2014-03-09 10:53:45 +0000

    Updated test.

--- example.pl
+++ example.pl
@@ -1,30 +1,48 @@
 #!/usr/bin/perl -Ilib/
-
+#
+#  Simple example test which connects to various hosts and tests
+# them.
+#
+# Steve
+# --
+#
 
 use strict;
 use warnings;
 
-use Test::More         tests => 5;
+use Test::More tests => 7;
 use Test::RemoteServer;
 
 #
-# Change each timeout to 2 seconds, from the default of 5.
+#  Some pre-test information.
 #
-$Test::RemoteServer::TIMEOUT = 2;
+print <<EOF;
+Running Test::RemoteServer version - $Test::RemoteServer::VERSION
+Timeouts are set to $Test::RemoteServer::TIMEOUT seconds
+
+EOF
 
 
-print "Running with $Test::RemoteServer::VERSION\n";
 
 ## testing ping responses
-ping_ok("localhost", "Localhost is dead?" );
-ping6_ok("localhost", "Localhost is dead?" );
+ping_ok( "localhost", "Localhost is dead?" );
+ping6_ok( "localhost", "Localhost is dead?" );
+
 
 ## There should be a HTTP server
 socket_open( "localhost", 80, "The webserver is dead!" );
 
-## Our domain shoudl resolve
-resolves( "example.com", "Our domain is unreachable!" );
+
+## FTP shouldn't be running
+socket_closed( "localhost", 21, "FTP should be disabled!" );
+
+
+## Our domain should resolve
+resolves( "steve.org.uk", "Our domain is unreachable!" );
+
 
 ## Test OpenSSH is secure.
-ssh_auth_disabled( "planet.debian-administration.org:2222", "password",
-                   "Password auth should be disabled");
+ssh_auth_disabled( "planet.debian-administration.org:2222",
+                   "password", "Password auth should be disabled" );
+ssh_auth_enabled( "planet.debian-administration.org:2222",
+                  "publickey", "Key auth is missing" );
